9978d704e50da62549dd072bff396fa197ac1705
FDuarte-Dev <fernando.guerra.duarte@gmail.com>
2022-07-19 17:12:07 +0100

    Fixed board limits

--- src/main/java/org/kata/marsRover/Board.java
+++ src/main/java/org/kata/marsRover/Board.java
@@ -2,7 +2,7 @@ package org.kata.marsRover;
 
 public class Board {
     public static final int NORTH_LIMIT = 9;
-    public static final int WEST_LIMIT = 9;
+    public static final int EAST_LIMIT = 9;
     public static final int SOUTH_LIMIT = 0;
-    public static final int EAST_LIMIT = 0;
+    public static final int WEST_LIMIT = 0;
 }
